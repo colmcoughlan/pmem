b05a48cb5d9989d7cffebb589fd004c2222fb73e
quentinmichelas <quentin-michelas@orange.fr>
2015-07-16 10:33:46 +0100

    Update task_pmem.py

--- CASA_interface/task_pmem.py
+++ CASA_interface/task_pmem.py
@@ -8,6 +8,7 @@ import os
 import pickle
 from taskinit import *
 
+
 def pmem(
               nbrpols,
               FilenameofI,
@@ -37,18 +38,27 @@ def pmem(
               outputname,
               ignoreedgepixels,
               debug):
+	casalog.filter('INFO')
+        casalog.origin('pmem')
 
 	l = [nbrpols, FilenameofI, FilenameofQ, FilenameofU, FilenameofV, Filenameofdirtybeam, FilenameofdefaultMap, Filenameofdefaultmask, estimatedspacingflux, conservefluxmode, rmsI, rmsQ, rmsU, rmsV, nbreofiteration, bmaj, bmin, bpa, blcx, blcy, trcx, trcy, accelerationfactor, qfactor, polarisationfactor, outputname, ignoreedgepixels, debug]
 
   
      
 
-	f = open('mempy_driver.dat','w')	
-        for i in range(len(l)):
+	f = open('mempy_driver.dat','w') # create a file named mempy_driver.dat
+        for i in range(len(l)):             # write in this file the parameters of the list l (one by line)
         	f.write(  str(l[i]) +'\n') 
          
         f.close()
-	os.system('/home/quentin/mem_code/pmem-master/pmem')
-   
-        casalog.origin('pmem')
+	os.system('/home/quentin/mem_code/pmem-master/pmem > pmem_log.txt') #close the file and execute the pmem code with the right link !
+	casalog.filter('INFO')
+	casalog.origin('pmem')
 	
+	g = open('pmem_log.txt','r')	
+	for ligne in g:
+    		para = ligne.strip()
+    		casalog.post(para)
+	g.close()
+
+   
